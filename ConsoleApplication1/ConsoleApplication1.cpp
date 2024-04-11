dbf922a4b35a977e18f8a723e275c71280aa6868
Roman 111 <romanvydysh7@gmail.com>
2024-04-11 20:07:49 +0200

    update

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream file("t.txt"); // Відкриття файлу для читання
+    if (!file.is_open()) {
+        std::cerr << "Не вдалося відкрити файл." << std::endl;
+        return 1;
+    }
+
+    std::string word;
+    std::string longestWord;
+    while (file >> word) { // Зчитування кожного слова з файлу
+        if (word.length() > longestWord.length()) { // Перевірка, чи є слово найдовшим
+            longestWord = word;
+        }
+    }
+
+    file.close(); // Закриття файлу
+
+    std::cout << "Word: " << longestWord << std::endl;
+    std::cout << "Max lenght word: " << longestWord.length() << std::endl;
+
+    return 0;
+}
